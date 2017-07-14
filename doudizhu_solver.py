1ae22267322735b3e593841191090bf2ed317c28
Carl.King <CarlKing5019@gmail.com>
2017-07-14 22:43:43 +0800

    重构代码: 1. 将王炸和一般炸弹统一处理 2. 将不同长度的顺子按不同牌型处理 3. 删除了出牌组合中的 sub 字段, 同牌型按 main 值比较大小 经过上述三步之后牌型字典和牌型大小比较函数都很简单
    4. 优化了模拟出牌过程的逻辑, 将转换出牌权的条件约化为两种情况
    5. 对于上一手为空的情形赋值 HAND_PASS, 方便统一处理
    6. 简化了缓存所用的 key 值, 并添加了打印中间过程的函数方便 debug

--- doudizhu_solver.py
+++ doudizhu_solver.py
@@ -5,24 +5,30 @@
 
 # 牌型枚举
 class COMB_TYPE:
-    PASS, SINGLE, PAIR, TRIPLE, TRIPLE_ONE, TRIPLE_TWO, FOURTH_TWO_ONES, FOURTH_TWO_PAIRS, STRIGHT, BOMB, KING_PAIR = range(11)
+    PASS, SINGLE, PAIR, TRIPLE, TRIPLE_ONE, TRIPLE_TWO, FOURTH_TWO_ONES, FOURTH_TWO_PAIRS, STRIGHT, BOMB = range(10)
 
 
-# 分别使用 3-17 代表 3-10, J, Q, K, A, 2, little_joker, big_joker
-cards = list(range(3,18))
-*_, little_joker, big_joker = cards
+# 3-14 分别代表 3-10, J, Q, K, A
+# 16, 18, 19 分别代表 2, little_joker, big_joker
+# 将 2 与其他牌分开是为了方便计算顺子
+# 定义 HAND_PASS 为过牌
+little_joker, big_joker = 18, 19
+HAND_PASS = {'type':COMB_TYPE.PASS, 'main': 0, 'component':[]}
 
 
-
-# 根据牌，获取此副牌所有可能的牌型
-# 牌型数据结构为牌类型，主牌，副牌, 包含的牌
+# 根据当前手牌，获取此牌所有可能出的牌型
+# 牌型数据结构为 {牌类型，主牌，包含的牌}
+# 同种牌类型可以通过主牌比较大小
+# 为方便比较大小, 将顺子按照不同长度分为不同牌型
 def get_all_hands(pokers):
     if not pokers:
         return []
 
-    combs = [{'type':COMB_TYPE.PASS}]
+    # 过牌
+    combs = [HAND_PASS]
 
 
+    # 获取每个点数的数目
     dic = {}
     for poker in pokers:
         dic[poker] = pokers.count(poker)
@@ -30,7 +36,7 @@ def get_all_hands(pokers):
 
     # 王炸
     if little_joker in pokers and big_joker in pokers:
-        combs.append({'type':COMB_TYPE.KING_PAIR, 'component': [little_joker, big_joker]})
+        combs.append({'type':COMB_TYPE.BOMB, 'main': big_joker, 'component': [big_joker, little_joker]})
 
 
     # 非顺子, 非王炸
@@ -49,10 +55,10 @@ def get_all_hands(pokers):
             for poker2 in dic:
                 if ALLOW_THREE_ONE and dic[poker2] >= 1 and poker2 != poker:
                     # 三带一
-                    combs.append({'type':COMB_TYPE.TRIPLE_ONE, 'main':poker, 'sub':poker2, 'component': [poker, poker, poker, poker2]})
+                    combs.append({'type':COMB_TYPE.TRIPLE_ONE, 'main':poker, 'component': [poker, poker, poker, poker2]})
                 if ALLOW_THREE_TWO and dic[poker2] >= 2 and poker2 != poker:
                     # 三带二
-                    combs.append({'type':COMB_TYPE.TRIPLE_TWO, 'main':poker, 'sub':poker2, 'component': [poker, poker, poker, poker2, poker2]})
+                    combs.append({'type':COMB_TYPE.TRIPLE_TWO, 'main':poker, 'component': [poker, poker, poker, poker2, poker2]})
 
         if dic[poker] == 4:
             # 炸弹
@@ -65,31 +71,37 @@ def get_all_hands(pokers):
                         ones.append(poker2)
                     elif dic[poker2] == 2:
                         pairs.append(poker2)
+
                 # 四带二单
                 for i in range(len(ones)):
                     for j in range(i + 1, len(ones)):
-                        combs.append({'type':COMB_TYPE.FOURTH_TWO_ONES, 'main':poker, 'sub1':ones[i], 'sub2':ones[j], 'component':[poker, poker, poker, poker, ones[i], ones[j]]})
+                        combs.append({'type':COMB_TYPE.FOURTH_TWO_ONES, 'main':poker, \
+                            'component':[poker, poker, poker, poker, ones[i], ones[j]]})
+
                 # 四带二对
                 for i in range(len(pairs)):
-                    combs.append({'type':COMB_TYPE.FOURTH_TWO_ONES, 'main':poker, 'sub1':pairs[i], 'sub2':pairs[i], 'component': [poker, poker, poker, poker, pairs[i], pairs[i]]})
+                    combs.append({'type':COMB_TYPE.FOURTH_TWO_ONES, 'main':poker, \
+                        'component': [poker, poker, poker, poker, pairs[i], pairs[i]]})
                     for j in range(i + 1, len(pairs)):
-                        combs.append({'type':COMB_TYPE.FOURTH_TWO_PAIRS, 'main':poker, 'sub1':pairs[i], 'sub2':pairs[j], 'component': [poker, poker, poker, poker, pairs[i], pairs[i], pairs[j], pairs[j]]})
+                        combs.append({'type':COMB_TYPE.FOURTH_TWO_PAIRS, 'main':poker, \
+                            'component': [poker, poker, poker, poker, pairs[i], pairs[i], pairs[j], pairs[j]]})
 
 
     # 所有顺子组合
-    sorted_set_of_pokers = sorted(list(set(pokers)))
-    for straight in create_straight(sorted_set_of_pokers, 5):
-        combs.append({'type':COMB_TYPE.STRIGHT, 'main':len(straight), 'sub': straight[-1], 'component': straight})
+    # 以 COMB_TYPE.STRIGHT * len(straight) 标志顺子牌型, 不同长度的顺子是不同的牌型
+    for straight in create_straight(list(set(pokers)), 5):
+        combs.append({'type':COMB_TYPE.STRIGHT * len(straight), 'main': straight[0], 'component': straight})
 
 
     # 返回所有可能的出牌类型
     return combs
 
 
-# 根据有序列表创建顺子
-def create_straight(sorted_list, min_length):
 
-    a = sorted_list
+# 根据列表创建顺子
+def create_straight(list_of_nums, min_length):
+
+    a = sorted(list_of_nums)
     lens = len(a)
     for start in range(0, lens):
         for end in range(start, lens):
@@ -101,32 +113,25 @@ def create_straight(sorted_list, min_length):
 
 
 # comb1 先出，问后出的 comb2 是否能打过 comb1
-def can_comb2_beat_comb1(comb1, comb2):
-    if comb2['type'] == COMB_TYPE.PASS:
+# 1. 同种牌型比较 main 值, main 值大的胜
+# 2. 炸弹大过其他牌型
+# 3. 牌型不同, 后出为负
+def can_beat(comb1, comb2):
+    if not comb2 or comb2['type'] == COMB_TYPE.PASS:
         return False
-
     if not comb1 or comb1['type'] == COMB_TYPE.PASS:
         return True
-
-    if comb1['type'] == comb2['type']:
-        if comb1['type'] == COMB_TYPE.STRIGHT:
-            if comb1['main'] != comb2['main']:
-                return False
-            else:
-                return comb2['sub'] > comb1['sub']
-        else:
-            if comb1['main'] == comb2['main']:
-                return comb2['sub'] > comb1['sub']
-            else:
-                return comb2['main'] > comb1['main']
-    elif comb2['type'] == COMB_TYPE.BOMB or comb2['type'] == COMB_TYPE.KING_PAIR:
-        return comb2['type'] > comb1['type']
+    elif comb1['type'] == comb2['type']:
+        return comb2['main'] > comb1['main']
+    elif comb2['type'] == COMB_TYPE.BOMB:
+        return True
     else:
         return False
 
 
 
 # 给定 pokers，求打出手牌 hand 后的牌
+# 用 component 字段标志打出的牌, 可以方便地统一处理
 def make_hand(pokers, hand):
     poker_clone = pokers[:]
     for poker in hand['component']:
@@ -136,28 +141,44 @@ def make_hand(pokers, hand):
 
 
 # 模拟每次出牌, my_pokers 为当前我的牌, enemy_pokers 为对手的牌
-# last_hand 为上一手的手牌
-def hand_out(my_pokers, enemy_pokers, last_hand, cache):
+# last_hand 为上一手对手出的牌, cache 用于缓存牌局与胜负关系
+def hand_out(my_pokers, enemy_pokers, last_hand = None, cache = {}):
+
+    # 牌局终止的边界条件
     if not my_pokers:
         return True
     if not enemy_pokers:
         return False
-    situation = ','.join([str(my_pokers),str(enemy_pokers), str(last_hand)])
-    if situation in cache:
-        return cache[situation]
-    all_hands = get_all_hands(my_pokers)
-    for hand in all_hands:
-        if (last_hand and can_comb2_beat_comb1(last_hand, hand)) or (not last_hand and hand['type'] != COMB_TYPE.PASS):
-            if not hand_out(enemy_pokers, make_hand(my_pokers, hand), hand, cache):
-                cache[situation] = True
+
+    # 如果上一手为空, 则将上一手赋值为 HAND_PASS
+    if last_hand is None:
+        last_hand = HAND_PASS
+
+    # 从缓存中读取数据
+    key = str((my_pokers, enemy_pokers, last_hand['component']))
+    if key in cache:
+        return cache[key]
+
+    # 模拟出牌过程, 深度优先搜索, 找到赢的分支则返回 True
+    for current_hand in get_all_hands(my_pokers):
+        # 转换出牌权有两种情况: 
+        # 1. 当前手胜出, 则轮到对方选择出牌
+        # 2. 当前手 PASS, 且对方之前没有 PASS, 则轮到对方出牌
+        if can_beat(last_hand, current_hand) or \
+        (last_hand['type'] != COMB_TYPE.PASS and current_hand['type'] == COMB_TYPE.PASS):
+            if not hand_out(enemy_pokers, make_hand(my_pokers, current_hand), current_hand, cache):
+                # print(True,' :', key)
+                cache[key] = True
                 return True
-        elif last_hand and hand['type'] == COMB_TYPE.PASS:
-            if not hand_out(enemy_pokers, my_pokers, None, cache):
-                cache[situation] = True
-                return True
-    cache[situation] = False
+
+    # 遍历所有情况, 均无法赢, 则返回 False
+    # print(False, ':', key)
+    cache[key] = False
     return False
 
+
+# todo:
+# 1. 用出牌列表作为 last_hand 的值, 方便调用函数
 
 
 if __name__ == '__main__':
@@ -168,14 +189,14 @@ if __name__ == '__main__':
     # 是否允许三带一
     ALLOW_THREE_ONE = True
     # 是否允许三带二
-    ALLOW_THREE_TWO = True
+    ALLOW_THREE_TWO = False
     # 是否允许四带二
     ALLOW_FOUR_TWO = True
 
-    lord = [17,16,11,11,9,9,9]
+    lord = [19,18,11,11,9,9,9]
     # farmer = [3,3,3,3,4,5,6,7,10,10,14,14,14,14]
-    farmer = [15, 15, 15, 14, 14, 14]
-    result = hand_out(farmer, lord, None, {})
+    farmer = [16, 16, 16, 14, 14, 14, 3, 4, 5, 6, 7, 8, 9, 10]
+    result = hand_out(farmer, lord)
 
 
     # 残局2
@@ -188,7 +209,7 @@ if __name__ == '__main__':
 
     # lord = [14,14,11,11]
     # farmer = [16,13,13,13,12,12,12,10,10,9,9,8,8]
-    # result = hand_out(farmer, lord, None, {})
+    # result = hand_out(farmer, lord)
 
     elapsed = (time.clock() - start)
 
